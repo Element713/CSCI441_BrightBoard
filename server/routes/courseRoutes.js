bae8860b46507c5e514f179e2e61f909470bf2ba
Shadow Love-Erckert <srlove@mail.fhsu.edu>
2025-06-29 18:06:16 -0500

    routes adjustment

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -4,15 +4,14 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
 const authMiddleware = require('../middleware/authMiddleware');
-const { verifyUser } = require('../middleware/roleMiddleware');
-const { verifyInstructor } = require('../middleware/roleMiddleware');
+const { verifyInstructor, verifyStudent } = require('../middleware/roleMiddleware');
 
 // Protected routes
-router.post('/', roleMiddleware.verifyInstructor, courseController.createCourse);
+router.post('/', verifyInstructor, courseController.createCourse);
 router.get('/', courseController.getCourses);
 router.get('/:id', courseController.getCourseById);
-router.put('/:id', roleMiddleware.verifyInstructor, courseController.updateCourse);
-router.delete('/:id', roleMiddleware.verifyInstructor, courseController.deleteCourse);
-router.post('/:id/enroll', roleMiddleware.verifyStudent, courseController.enrollInCourse);
+router.put('/:id', verifyInstructor, courseController.updateCourse);
+router.delete('/:id', verifyInstructor, courseController.deleteCourse);
+router.post('/:id/enroll', verifyStudent, courseController.enrollInCourse);
 
 module.exports = router;
